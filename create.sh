cf495edae386bd436a92c7e7c955026ff5dc555a
yves dobozy <yves.dobozy@ucdconnect.ie>
2020-11-27 14:10:13 +0000

    testing space

--- create.sh
+++ create.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-user=$1
+user="$1"
 
 ##check arguments
-if [ ! $# -eq 1 ]; then
+if [ ! "$#" -eq 1 ]; then
 	echo "Error: parameters problem" >&2
 	exit 1
 
 ##check if user already exists
-	elif [ -d $user ]; then
+	elif [ -d "$user" ]; then
 	echo "Error: user already exists" >&2
 	exit 1
 fi
 
-mkdir $user
-touch $user/friends
-touch $user/wall 
+mkdir "$user"
+touch "$user"/friends
+touch "$user"/wall 
 echo "OK: user created"
 exit 0
